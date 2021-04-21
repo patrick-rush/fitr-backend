d785934256c85c6065b28af9fb0b3769eb60c72a
patrick-rush <patrick.a.rush@gmail.com>
2021-04-21 19:42:14 -0400

    add workout relationship to exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,6 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+  has_many :exercises, through: :workout_exercises
 
   validates :name, presence: true
   validates :category, inclusion: { in: %w(endurance strength balance flexability) }
