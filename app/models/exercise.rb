b686774827f45e8dc19a3f3f7b90a8c540e4d238
patrick-rush <patrick.a.rush@gmail.com>
2021-04-21 19:02:18 -0400

    add validations to Exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,8 @@
 class Exercise < ApplicationRecord
   belongs_to :user
   belongs_to :workout
+
+  validates :name, presence: true
+  validates :category, inclusion: { in: %w(endurance strength balance flexability) }
+  validates :target, inclusion: { in: %w(chest back arms shoulders legs core other) } 
 end
