126893fdaf2dcdf889f271e63bad48b044cd0afc
patrick-rush <patrick.a.rush@gmail.com>
2021-04-22 20:06:58 -0400

    update model relationship

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,6 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+  has_many :workout_exercises
   has_many :workouts, through: :workout_exercises
 
   validates :name, presence: true
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,7 @@
 class Workout < ApplicationRecord
   belongs_to :user
+  has_many :workout_exercises
   has_many :exercises, through: :workout_exercises
 
-  validates_presence_of :exercise
+  # validates_presence_of :exercise
 end
