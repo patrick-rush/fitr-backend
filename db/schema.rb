bd06ea53d4c66a4dde17a37e181f3a8f24de9883
branmar97 <brandonmar97@gmail.com>
2021-04-26 20:15:18 -0400

    add club model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_233332) do
+ActiveRecord::Schema.define(version: 2021_04_27_001117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clubs", force: :cascade do |t|
+    t.string "uname"
+    t.string "category"
+    t.text "description"
+    t.string "location"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_clubs_on_user_id"
+  end
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
@@ -60,6 +71,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_233332) do
     t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
+  add_foreign_key "clubs", "users"
   add_foreign_key "exercises", "users"
   add_foreign_key "workout_exercises", "exercises"
   add_foreign_key "workout_exercises", "workouts"
