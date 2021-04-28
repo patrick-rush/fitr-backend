0d99244428ac7d9047dfc8cdde686a896af6ac46
patrick-rush <patrick.a.rush@gmail.com>
2021-04-27 22:31:01 -0400

    remove nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts do
-    resources :comments, only: [:index]
-  end
+  resources :posts
   resources :exercises
   resources :workouts
   get '/current_user', to: 'current_user#index'
