4e953db4aa42d9a1526c91f0f18d2b0f3dc9c16e
patrick-rush <patrick.a.rush@gmail.com>
2021-04-28 23:08:13 -0400

    add has_many likes relationships

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   # has_many :likes, as: :likeable
   has_many :comments
+  has_many :likes, as: :likeable
   belongs_to :user
 
   validates :content, presence: true
